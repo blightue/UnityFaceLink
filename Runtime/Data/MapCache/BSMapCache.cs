b5c20377a19a7848873b531cab77729352c44e6a
suisuishou <blightue@gmail.com>
2023-08-17 17:07:36 +0800

    Add evaluate bs

--- Runtime/Data/MapCache/BSMapCache.cs
+++ Runtime/Data/MapCache/BSMapCache.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace FaceLink.Data
 {
     public abstract class BSMapCacheAbstract
     {
         public abstract IEnumerable<BSSkMRPairsCache> PairsCaches { get; }
+        public abstract void EvaluateBS(float[] bsvalues, SkinnedMeshRenderer[] targets);
     }
-    
+
     [Serializable]
-    public class BSMapCache: BSMapCacheAbstract
+    public class BSMapCache : BSMapCacheAbstract
     {
         public override IEnumerable<BSSkMRPairsCache> PairsCaches => _pairsCaches;
         public BSSkMRPairsCache[] _pairsCaches;
+
+        public override void EvaluateBS(float[] bsvalues, SkinnedMeshRenderer[] targets)
+        {
+            if (_pairsCaches.Length != targets.Length) return;
+            for (int i = 0; i < _pairsCaches.Length; i++)
+            {
+                BSSkMRPairsCache cache = _pairsCaches[i];
+                SkinnedMeshRenderer renderer = targets[i];
+                for (var j = 0; j < bsvalues.Length; j++)
+                {
+                    int bsIndex = cache.ARkit2SkMRIndiceMap[j];
+                    if (bsIndex == -1) continue;
+                    renderer.SetBlendShapeWeight(bsIndex, bsvalues[j] * 100f);
+                }
+            }
+        }
     }
 
     [Serializable]
@@ -26,6 +44,23 @@ namespace FaceLink.Data
     {
         public override IEnumerable<BSSkMRPairsCache> PairsCaches => _pairsCaches;
         public BSSkMRStrengthPairsCache[] _pairsCaches;
+
+        public override void EvaluateBS(float[] bsvalues, SkinnedMeshRenderer[] targets)
+        {
+            if (_pairsCaches.Length != targets.Length) return;
+            for (int i = 0; i < _pairsCaches.Length; i++)
+            {
+                BSSkMRStrengthPairsCache cache = _pairsCaches[i];
+                SkinnedMeshRenderer renderer = targets[i];
+                for (var j = 0; j < bsvalues.Length; j++)
+                {
+                    int bsIndex = cache.ARkit2SkMRIndiceMap[j];
+                    if (bsIndex == -1) continue;
+                    float strength = cache.ARKit2SkMRStrengthMap[j];
+                    renderer.SetBlendShapeWeight(bsIndex, bsvalues[j] * strength * 100f);
+                }
+            }
+        }
     }
 
     [Serializable]
